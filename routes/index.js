a98efe770ba59ceb0e1f6e27c5c165bc4939e585
Dillon Merritt <merrittd42@gmail.com>
2017-11-01 17:49:23 -0400

    Added routes, installed multer to handle file uploads.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'encrypt.io' });
+});
+
+router.post('/profile', upload.single('encryptFile'), async (req, res) => {
+
 });
 
 module.exports = router;
