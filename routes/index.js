ee958329b2f5525646b62447f03e863b0ffc979a
Dillon Merritt <merrittd42@gmail.com>
2017-11-01 17:59:05 -0400

    Multer not working

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,35 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
-
+var app = express()
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'encrypt.io' });
 });
 
-router.post('/profile', upload.single('encryptFile'), async (req, res) => {
+app.post('/upload/encrypt/caesar', upload.single('encryptFile'), function (req, res, next){
+  var file = req.file;
+  console.log(req.file)
+})
 
-});
+router.post('/upload/encrypt/pad', upload.single('encryptFile'), async (req, res) => {
+
+})
+
+router.post('/upload/encrypt/AES', upload.single('encryptFile'), async (req, res) => {
+
+})
+
+router.post('/upload/decrypt/caesar', upload.single('decryptFile'), async (req, res) => {
+
+})
+
+router.post('/upload/decrypt/pad', upload.single('decryptFile'), async (req, res) => {
+
+})
+
+router.post('/upload/decrypt/AES', upload.single('decryptFile'), async (req, res) => {
+
+})
 
 module.exports = router;
