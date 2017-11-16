bfaa3ef032abd4c57d57f7b7c95939b4d57544a5
Brian Baker <bbaker5245@yahoo.com>
2017-11-16 18:06:58 -0500

    one time pad encryption/decryption both working

--- crypto/onetimepad.js
+++ crypto/onetimepad.js
@@ -9,41 +9,35 @@ module.exports = {
 	  the key for decryption
     */
     encryptPad : function(fileLoc){
-      var outputFileLoc = fileLoc + '.enc';
-      var fs = require('fs');
+	  var outputFileLoc = fileLoc + '.enc';
+	  var fs = require('fs');
 
-      var plainText = fs.readFileSync(fileLoc); // read plain text from input file
-      var cipherText = '';
-      var padKey = '';
+	  var plainText = fs.readFileSync(fileLoc); // read plain text from input file
+	  var cipherText = '';
+	  var padKey = '';
 
-      // perform encryption one character at a time
-      for (var i = 0, len = plainText.length; i < len; i++) {
-        var nextChar = '';
-        var byteCode = plainText[i]; // get byte code for this character
+	  // perform encryption one character at a time
+	  for (var i = 0, len = plainText.length; i < len; i++) {
+		var byteCode = plainText[i]; // get byte code for this character
+		var shiftAmount = Math.floor(Math.random() * 95); // gets a number between 0 and 94
+		var encryptedCode;
+		
+		if(byteCode < 32 || byteCode > 126){
+			encryptedCode = byteCode;
+		}else{
+			encryptedCode = ((byteCode - 32 + shiftAmount) % 95) + 32;
+		}
+		
+		padKey += String.fromCharCode(shiftAmount + 32); // slowly create the pad key
+		cipherText += String.fromCharCode(encryptedCode); // append the (possibly encrypted) char to the cipher text
+	  }
 
-        var shiftAmount = Math.floor(Math.random() * 26) + 1; // gets a number between 1 and 26
-
-        if(byteCode >= 65 && byteCode <= 90){ // character is an upper case letter
-          encryptedCode = ((byteCode - 65 + shiftAmount) % 26) + 65;
-          nextChar = String.fromCharCode(encryptedCode);
-        }else if(byteCode >= 97 && byteCode <= 122){ // character is a lower case letter
-          encryptedCode = ((byteCode - 97 + shiftAmount) % 26) + 97;
-          nextChar = String.fromCharCode(encryptedCode);
-        }else{ // character is not a letter
-          nextChar = String.fromCharCode(plainText[i]);
-        }
-
-        padKey += shiftAmount.toString(); // slowly create the pad key
-
-        cipherText += nextChar; // append the (possibly encrypted) char to the cipher text
-      }
-
-      // write cipher text to output file location
-      fs.writeFileSync(outputFileLoc, cipherText,  function(err) {
-        if (err) {
-          return console.error(err);
-        }
-      });
+	  // write cipher text to output file location
+	  fs.writeFileSync(outputFileLoc, cipherText,  function(err) {
+		if (err) {
+		  return console.error(err);
+		}
+	  });
 
       return [outputFileLoc, padKey]; // return location of encrypted folder
     },
@@ -58,7 +52,32 @@ module.exports = {
 
     */
     decryptPad: function(fileLoc, pad){
+		var fs = require('fs');	
+		var outputFileLoc = fileLoc + '.dec';
 
+		var cipherText = fs.readFileSync(fileLoc); // read cipher text from input file
+		var plainText = '';
+
+		// perform encryption one character at a time
+		for (var i = 0, len = cipherText.length; i < len; i++) {
+			var byteCode = cipherText[i]; // get byte code for this character
+			
+			if(byteCode < 32 || byteCode > 126){
+				plainText += String.fromCharCode(byteCode); // append the (possibly encrypted) char to the cipher text
+			}else{
+				var shiftAmount = pad.charCodeAt(i) - 32;
+				var decryptedCode = ((byteCode - 32 - shiftAmount + 95) % 95) + 32;
+				plainText += String.fromCharCode(decryptedCode); // append the (possibly encrypted) char to the cipher text
+			}
+		}
+
+		// write cipher text to output file location
+		fs.writeFileSync(outputFileLoc, plainText,  function(err) {
+			if (err) {
+			  return console.error(err);
+			}
+		});
+
+		return outputFileLoc; // return location of encrypted file
     }
-
 };
